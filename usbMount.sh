03dff00789db2e08cde093710c6bd62faca38ff3
siva1024 <31105586+siva1024@users.noreply.github.com>
2019-10-31 20:09:03 -0700

    Update usbMount.sh

--- usbMount.sh
+++ usbMount.sh
@@ -1,1 +1,17 @@
+# Install the utils for the exfat
+sudo apt-get install exfat-fuse exfat-utils
 
+# List out all the drives that are available
+lsblk
+
+# Or
+sudo fdisk -l
+
+# Create a directory for the mount point
+sudo mkdir /media/usb
+
+# Mount the usb to the mount point
+sudo mount /dev/sda1 /media/usb
+
+# unmount the usb
+sudo umount /media/usb
