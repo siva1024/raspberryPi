8ee681d17735bf5052366fce36a800841db20757
siva1024 <31105586+siva1024@users.noreply.github.com>
2019-11-01 21:44:21 -0700

    Update sambaFileShare.sh

--- sambaFileShare.sh
+++ sambaFileShare.sh
@@ -5,16 +5,16 @@ sudo apt-get install samba samba-common-bin
 sudo nano /etc/samba/smb.conf
 
 # At the end of the file add the following lines
-[share]
-   Comment = Pi Shared Folder
-   Path = /media/usb
-   Browseable = yes
-   Writeable = Yes
-   only guest = no
+
+[master]
+   comment = Pi Shared Folder
+   path = /media/usb
+   browseable = yes
+   valid user = pi
+   writeable = Yes
    create mask = 0777
    directory mask = 0777
-   Public = yes
-   Guest ok = yes
+   guest ok = no
    
 # Start the samba service
 sudo service smbd start
@@ -24,3 +24,6 @@ sudo service smbd stop
 
 # Restart the samba service
 sudo service smbd restart
+
+# Create a user Samba user
+sudo smbpasswd -a <User_name>
